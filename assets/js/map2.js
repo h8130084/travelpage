ddb87e02f92e2cf578e7852a78a4445e17d87246
Tony <h8130084@live.tees.ac.uk>
2019-06-18 17:07:08 +0000

    made minor amendments on js file, styled the index page with css

--- assets/js/map2.js
+++ assets/js/map2.js
@@ -1,4 +1,13 @@
-
+// Create a site that calls on the Google Maps API and/or 
+// the Google Places API (or similar) to allow users to
+// search for their next holiday destination. You'll want help your users:
+// Select a destination city
+// Find tourist attractions
+// Find accommodation
+// Find bars and restaurants
+// Provide search results in a manner that is visually appealing 
+// for your user (by drawing on the skills you have
+// learned in User-Centric Frontend Development)
 //make check box look better?? //
 //sort css //
 //sort page lay out//
